b94af9cae331a3c2c6466e85ff189e906eed098e
estudiante <universidadDeMedellin.com>
2026-09-14 19:11:44 -0500

    ..

--- lib/event_screen.dart
+++ lib/event_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:examen_practico_g61_starter/event.dart';
+import 'package:examen_practico_g61_starter/event_repository.dart';
 import 'package:flutter/material.dart';
 
 class EventScreen extends StatefulWidget {
@@ -8,8 +12,102 @@ class EventScreen extends StatefulWidget {
 }
 
 class _EventScreenState extends State<EventScreen> {
+  List<Event> eventos = [];
+  bool cargando = false;
+  bool errorCarga = false;
+  String errorMessage = "";
+
+  Future<String> loadMessage() async {
+    setState(() {
+      cargando = true;
+    });
+
+   try {
+      var resultado = await loadEvents(simulateError: false, simulateEmpty: false);
+      setState(() {
+        eventos = resultado;
+        cargando = false;
+      });
+      return "";
+    } catch (e) {
+    setState(() {
+      cargando = false;
+     
+    });
+    return 'No fue posible cargar';
+  }
+
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    loadMessage();
+    
+    if(cargando){
+      return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reserva de entradas'),
+      ),
+      body: Center(
+          child: Padding(
+            padding: EdgeInsets.all(24),
+            child: LoadingAnimationWidget.staggeredDotsWave(
+          color: Colors.white,
+          size: 200,
+        ),
+        ),
+      ),
+    );
+    }
+
+    if(errorCarga){
+      return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reserva de entradas'),
+      ),
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            children: [
+            
+            Text(
+              'Se ha producido un error durante la carga de datos',
+              textAlign: TextAlign.center,
+            )
+            ],
+          ),
+        ),
+      ),
+    );
+    }
+
+    if(eventos.isEmpty){
+
+      return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reserva de entradas'),
+      ),
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            children: [
+            
+            Text(
+              'No hay eventos disponibles',
+              textAlign: TextAlign.center,
+            )
+            ],
+          ),
+        ),
+      ),
+    );
+    }
+
+
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Reserva de entradas'),
@@ -17,11 +115,9 @@ class _EventScreenState extends State<EventScreen> {
       body: const Center(
         child: Padding(
           padding: EdgeInsets.all(24),
-          child: Text(
-            'Starter listo.\n\n'
-            'Construya aquí la solución solicitada en el examen.',
-            textAlign: TextAlign.center,
-          ),
+          child: Column(
+            children: eventos.map
+          )
         ),
       ),
     );
